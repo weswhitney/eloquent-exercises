c81f90539fd8113233de521a076d0a8334821e0a
weswhitney <wesleywhitney@gmail.com>
2015-05-27 19:44:40 -0600

    finished the second part of chess board

--- chess_board.js
+++ chess_board.js
@@ -1,5 +1,9 @@
-chessString = ' # # # #'
-chessBoard = '# # # # '
-for (var i = 0; i <= 3; i++) {
+
+chessString = ' # # # #';
+chessBoard = '# # # # ';
+size = 11;
+size = size/2;
+
+for (var i = 1; i <= size; i++) {
   console.log(chessString + "\n" + chessBoard)
 }
