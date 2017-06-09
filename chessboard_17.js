5086c0ba36443a19c1db26bf588cbec08b79226f
Wesley  Whitney <wesleywhitney@gmail.com>
2017-06-09 13:39:46 -0600

    new iteration

---
+++ chessboard_17.js
@@ -0,0 +1,7 @@
+var row1 = " # # # #\n";
+var row2 = "# # # # ";
+var result;
+for (var i = 0; i < 4; i++) {
+	result = row1 + row2;
+	console.log(result);
+}
---
+++ looping_a_triangle_17.js
@@ -0,0 +1,6 @@
+var pyramid = '';
+var hashTag = '#';
+for (var i = 0; i < 7; i++) {
+	pyramid = pyramid + hashTag;
+	console.log(pyramid);
+}	
