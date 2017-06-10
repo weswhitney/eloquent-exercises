c13462278151168c00bbb79ed31d699050454b7a
Wesley  Whitney <wesleywhitney@gmail.com>
2017-06-09 22:57:14 -0600

    finished chessboard exercise || brutal

--- chessboard_17.js
+++ chessboard_17.js
@@ -1,7 +1,19 @@
-var row1 = " # # # #\n";
-var row2 = "# # # # ";
-var result;
-for (var i = 0; i < 4; i++) {
-	result = row1 + row2;
-	console.log(result);
+// position variables
+var hash = '#';
+var space = ' ';
+var board = '';
+// grid variables
+var size = 81;
+var newLine = '\n';
+
+for (var i = 0; i < size; i++) {
+	for (var j = 0; j < size; j++) {
+		if ((j + i) % 2 === 0) {
+			board = board + space;
+		} else {
+			board = board + hash;
+		}
+	}
+	board = board + newLine;
 }
+	console.log(board);
