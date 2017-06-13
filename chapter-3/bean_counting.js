e16d53de0b53f5334f12ac59af9dfaff0b946498
Wesley  Whitney <wesleywhitney@gmail.com>
2017-06-13 08:54:18 -0600

    deleted cruft

--- chapter-3/bean_counting.js
+++ chapter-3/bean_counting.js
@@ -1,13 +1,3 @@
-function countBs(str) {
-	var count = 0;
-	for (var i = 0; i < str.length; i++) {
-		if (str.charCodeAt(i) === 66) {
-			count++;
-		}
-	}
-	console.log(count);
-};
-
 function countChar(str, character) {
 	var count = 0;
 	var charCode = character.charCodeAt(0);
@@ -18,6 +8,3 @@ function countChar(str, character) {
 	}
 	console.log(count);
 };
-
-countChar("sS bBBBBB", "s");
-
