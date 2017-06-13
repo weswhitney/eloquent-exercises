bb8040f3619ec2a3856613823c65d50ab2f8f097
Wesley  Whitney <wesleywhitney@gmail.com>
2017-06-12 20:37:34 -0600

    finished exercise

--- chapter-3/bean_counting.js
+++ chapter-3/bean_counting.js
@@ -7,3 +7,17 @@ function countBs(str) {
 	}
 	console.log(count);
 };
+
+function countChar(str, character) {
+	var count = 0;
+	var charCode = character.charCodeAt(0);
+	for (var i = 0; i < str.length; i++) {
+		if (str.charCodeAt(i) === charCode) {
+			count++;
+		}
+	}
+	console.log(count);
+};
+
+countChar("sS bBBBBB", "s");
+
