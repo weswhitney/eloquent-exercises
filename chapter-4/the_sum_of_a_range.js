93121ecdd59fd3c9d5a198825e7dd60b7e623bbe
Wesley  Whitney <wesleywhitney@gmail.com>
2017-06-15 07:36:04 -0600

    range function

--- chapter-4/the_sum_of_a_range.js
+++ chapter-4/the_sum_of_a_range.js
@@ -1,9 +1,24 @@
 function range(start, end) {
 	var rangeArr = [];
-	return rangeArr; // returns an array containing all the numbers from start up to (and including) end
+	if (start === end) {
+		return rangeArr;
+	}
+	var leftSide = Math.max(start, end);
+	var rightSide = Math.min(start, end);
+	var loops = leftSide - rightSide;
+	for (var i = 0; i <= loops; i++) {
+		var nextNum = 0;
+		nextNum = nextNum + rightSide;
+		rightSide += 1;
+		rangeArr.push(nextNum);
+	}
+	console.log(rangeArr);
+	// return rangeArr; // returns an array containing all the numbers from start up to (and including) end
 }
 
-function sum(numArr) {
-	var sum = 0;
-	return sum;
-}
+// function sum(numArr) {
+// 	var sum = 0;
+// 	return sum; // returns the sum of these numbers
+// }
+
+range(-1, -12);
