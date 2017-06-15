3a58e68a63c1487f39bfe12c6b8518ce3c07acef
Wesley  Whitney <wesleywhitney@gmail.com>
2017-06-14 22:28:04 -0600

    chapter 4 folder

---
+++ chapter-4/the_sum_of_a_range.js
@@ -0,0 +1,9 @@
+function range(start, end) {
+	var rangeArr = [];
+	return rangeArr; // returns an array containing all the numbers from start up to (and including) end
+}
+
+function sum(numArr) {
+	var sum = 0;
+	return sum;
+}
